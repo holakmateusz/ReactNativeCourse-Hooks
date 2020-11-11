5199f8d385e09d654476fb15c0f667fa012fbd9e
Holak Mateusz <holak.mateusz@gmail.com>
2020-11-11 22:10:26 +0100

    Replace useState with useReducer in CounterScreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, counter: state.counter + action.payload };
+    case "decrement":
+      return { ...state, counter: action.payload + state.counter };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
   return (
     <View>
-      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
-      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-      <Text>Current count:{counter}</Text>
+      <Button
+        title="Increase"
+        onPress={() => dispatch({ type: "increment", payload: 1 })}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => dispatch({ type: "decrement", payload: -1 })}
+      />
+      <Text>Current count:{state.counter}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default CounterScreen;
